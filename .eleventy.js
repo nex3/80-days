71f29437620c6622345234a1d72a954955315ea6
Natalie Weizenbaum <nweiz@google.com>
2022-08-02 22:48:03 -0700

    Add a favicon

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,10 @@
 module.exports = function (config) {
     config.setUseGitIgnore(false);
 
-    // config.addPassthroughCopy({ "source/images/*": "/images" });
+    config.addPassthroughCopy({ "source/images/*": "/images" });
 
     if (process.env.NODE_ENV === "dev") {
         config.addPassthroughCopy({ "source/css": "/css" }); // uncompiled
-        config.addPassthroughCopy({ "build/images": "/images" });
     } else {
         // Compiled CSS is output directly to the build directory with PostCSS
     }
