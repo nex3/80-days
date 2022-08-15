522586c3c9f648655704570a5e4cb12f8912b281
Natalie Weizenbaum <nweiz@google.com>
2022-08-15 16:40:17 -0700

    Increase distribution of cities with multiple players

--- source/js/main.ts
+++ source/js/main.ts
@@ -73,7 +73,7 @@ const pathsByPlayerEntity = new Map<Cesium.Entity, Cesium.Entity>();
 const playersByEntity = new Map<Cesium.Entity, Player>();
 for (const player of players) {
   const count = playersByCity[player.city.name!].length;
-  const r = player.progress || count === 1 ? 0 : Math.random() * (6 + count);
+  const r = player.progress || count === 1 ? 0 : Math.random() * (10 + count);
   const theta = Math.random() * 2 * Math.PI;
   const playerEntity = viewer.entities.add({
     name: player.name,
